a0d117ca1bb29508f1414bb3950a0fbbd96645f7
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2024-05-20 12:21:43 +0000

    creation + json of ehb

---
+++ sailing-bingo/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Board from './components/Board';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Old School RuneScape Event Board</h1>
+      </header>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
---
+++ sailing-bingo/src/components/Board.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/Board.css';
+
+const Board = () => {
+
+  return (
+    <div className="board">
+      
+    </div>
+  );
+};
+
+export default Board;
